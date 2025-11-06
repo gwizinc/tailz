900015b574960b41aaf457a6a33e7083a7c36ded
Steve^ <steve@stevepeak.net>
2025-11-06 12:46:17 +0700

    remove not used stuff

--- apps/trigger/src/index.ts
+++ apps/trigger/src/index.ts
@@ -3,8 +3,6 @@ import '@trigger.dev/sdk'
 // Export tasks from local task definitions
 export { findStoriesInCommitTask } from './tasks/find-stories-in-commit'
 export { updateGithubStatusTask } from './tasks/update-github-status'
-export { syncGithubInstallationTask } from './tasks/sync-github-installation'
-export { setEnabledReposTask } from './tasks/set-enabled-repos'
 export { testHelloWorldTask } from './tasks/test-hello-world'
 
 // Export helper types and functions
